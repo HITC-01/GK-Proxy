a35e538961436ec8350d16b349bae7827509441e
gkim795 <glory@g.com>
2018-11-07 19:08:39 -0800

    centered and uploaded

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const apiProxy = httpProxy.createProxyServer();
 const serverOne = 'http://localhost:3001';  //serves user profile
 const serverTwo = 'http://localhost:3003';  //serves user comments
-const serverThree = 'http://localhost:3002'; //serves related tracks
+// const serverThree = 'http://localhost:3002'; //serves related tracks
 const serverFour = 'http://localhost:3004'; //serves the sound cloud player
 
 const app = express();
@@ -17,19 +17,19 @@ app.all("/user/*", function(req, res) {
  apiProxy.web(req, res, {target: serverOne});
 });
 
-app.all("/api/sc/songs/:songId", function(req, res) {
+app.all("/comments/*", function(req, res) {
  console.log('redirecting to Server2');
  apiProxy.web(req, res, {target: serverTwo});
 });
 
-app.all("/songs/:songId/related", function(req, res) {
- console.log('redirecting to Server3');
- apiProxy.web(req, res, {target: serverThree});
-});
-app.all("/other/:songId", function(req, res) {
-  console.log('redirecting to Server3');
-  apiProxy.web(req, res, {target: serverThree});
- });
+// app.all("/songs/:songId/related", function(req, res) {
+//  console.log('redirecting to Server3');
+//  apiProxy.web(req, res, {target: serverThree});
+// });
+// app.all("/other/:songId", function(req, res) {
+//   console.log('redirecting to Server3');
+//   apiProxy.web(req, res, {target: serverThree});
+//  });
 
 app.all("/player/*", function(req, res) {
  console.log('redirecting to Server4');
