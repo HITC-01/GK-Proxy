a54ba41a21f122be9ea6e53950790ccda6e4de3d
gkim795 <glory@g.com>
2018-11-07 19:28:58 -0800

    added all four

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const apiProxy = httpProxy.createProxyServer();
 const serverOne = 'http://localhost:3001';  //serves user profile
 const serverTwo = 'http://localhost:3003';  //serves user comments
-// const serverThree = 'http://localhost:3002'; //serves related tracks
+const serverThree = 'http://localhost:3002'; //serves related tracks
 const serverFour = 'http://localhost:3004'; //serves the sound cloud player
 
 const app = express();
@@ -22,14 +22,11 @@ app.all("/comments/*", function(req, res) {
  apiProxy.web(req, res, {target: serverTwo});
 });
 
-// app.all("/songs/:songId/related", function(req, res) {
-//  console.log('redirecting to Server3');
-//  apiProxy.web(req, res, {target: serverThree});
-// });
-// app.all("/other/:songId", function(req, res) {
-//   console.log('redirecting to Server3');
-//   apiProxy.web(req, res, {target: serverThree});
-//  });
+app.all("/related/*", function(req, res) {
+ console.log('redirecting to Server3');
+ apiProxy.web(req, res, {target: serverThree});
+});
+
 
 app.all("/player/*", function(req, res) {
  console.log('redirecting to Server4');
