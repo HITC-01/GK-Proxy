67bcd343cbc8e6f3ce4ebfad9d6c024232d0fff7
gkim795 <glory@g.com>
2018-11-10 18:16:49 -0800

    proxy complete

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const httpProxy = require('http-proxy');
 const port = 3000;
 
 const apiProxy = httpProxy.createProxyServer();
-const serverOne = 'http://localhost:3001';  //serves user profile
-const serverTwo = 'http://localhost:3003';  //serves user comments
-const serverThree = 'http://localhost:3002'; //serves related tracks
-const serverFour = 'http://localhost:3004'; //serves the sound cloud player
+const serverOne = 'http://ec2-13-57-241-194.us-west-1.compute.amazonaws.com';  //serves user profile
+const serverTwo = 'http://ec2-54-185-4-172.us-west-2.compute.amazonaws.com/';  //serves user comments
+const serverThree = 'http://ec2-54-215-217-201.us-west-1.compute.amazonaws.com/'; //serves related tracks
+const serverFour = 'http://ec2-18-222-200-123.us-east-2.compute.amazonaws.com/'; //serves the sound cloud player
 
 const app = express();
 
